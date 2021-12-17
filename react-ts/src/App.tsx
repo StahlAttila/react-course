97248846df19436d91210ba7a99c9e7ad6ab7feb
StahlAttila <stattilasd@gmail.com>
2021-12-17 16:27:06 +0100

    add Todos component

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css';
+import Todos from './components/Todos';
 
 function App() {
   return (
     <div>
-      ASD
+      <Todos items={['asd1', 'asd2']}/>
     </div>
   );
 }
---
+++ react-ts/src/components/Todos.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Todos: React.FC<{items: string[]}> = (props) => {
+  return (
+    <div>
+      {props.items.map(item => <li key={item}>{item}</li>)}
+    </div>
+  )
+}
+
+export default Todos
+
